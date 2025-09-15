46ddb4a900f814fd8b7924407a92d4d815ab7479
Przemysław Baśkiewicz <przemyslaw.baskiewicz@gmail.com>
2025-09-15 12:08:36 +0200

    Create __start.cpp

---
+++ __start.cpp
@@ -0,0 +1,15 @@
+// Pliki naglowkow
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+
+// funkcja main jest punktem startowym programu
+int main() {
+
+    // tu wpisujemy kod programu
+
+    return 0; // infromacja dla systemu, ze wszystko ok
+}
